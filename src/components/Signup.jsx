679a7673c80027ccba3141f47c31ef992e2b2ecd
Varun-18 <109971426+Varun-18@users.noreply.github.com>
2023-06-09 15:58:59 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -93,7 +93,7 @@ const Signup = () => {
             </label>
             <input
               {...register("password")}
-              type="text"
+              type="password"
               id="password"
               name="password"
               className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
